b57a5d9028156e7a6cdf559b91c596d5cf5060cc
hqakiet <kiethqa051197@gmail.com>
2026-04-12 22:17:43 +0700

    edit css

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/chua-chan-roaster',
+  base: '/chua-chan-roaster',
 })
